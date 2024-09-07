547aa1a0c189e3b1d24992c6f66829f96f1083bf
Danilo Campos <108036394+dcampos90@users.noreply.github.com>
2024-09-07 17:35:35 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,72 @@
+let dados = [
+    {
+        titulo: "Turma da Mônica: Laços",
+        linktitulo: "http://www.guiadosquadrinhos.com/edicao/graphic-msp-(capa-dura)-n-2/gr011105/105052",
+        descricao: "A graphic novel 'Turma da Mônica: Laços' marca um novo capítulo na história dos personagens mais queridos dos quadrinhos brasileiros. Com uma narrativa emocionante e uma arte impecável, a obra resgata a essência da infância e da amizade, transportando os leitores para um universo repleto de aventuras e descobertas. A trama, centrada no desaparecimento do cãozinho Floquinho, explora temas como a importância da amizade, a superação de desafios e a valorização dos laços afetivos. A graphic novel, além de divertir, promove reflexões sobre a importância dos amigos e da união para superarmos qualquer obstáculo.",  
+        link: "https://pt.wikipedia.org/wiki/Turma_da_M%C3%B4nica:_La%C3%A7os",
+        tags: "mauricio monica cebolinha magali cascao"
+    },
+    {
+        titulo: "Turma da Mônica: Lições",
+        linktitulo: "http://www.guiadosquadrinhos.com/edicao/graphic-msp-(capa-dura)-n-8/gr011105/118457",
+        descricao: "Turma da Mônica: Lições é uma emocionante graphic novel da série Graphic MSP que explora o universo dos personagens clássicos de Mauricio de Sousa sob uma nova perspectiva. Nessa história, Mônica, Cebolinha, Magali e Cascão enfrentam desafios e aprendem lições importantes sobre amizade, respeito e cooperação. A obra, escrita por Vitor Caffagi e ilustrada por Lu Cafaggi, aborda temas do dia a dia de forma leve e divertida, tornando a leitura uma experiência enriquecedora para crianças e adultos. A graphic novel, que faz parte da trilogia iniciada por 'Laços', mostra que mesmo os melhores amigos podem passar por momentos de desentendimento, mas a amizade verdadeira sempre prevalece.",
+        link: "https://pt.wikipedia.org/wiki/Turma_da_M%C3%B4nica:_Li%C3%A7%C3%B5es",
+        tags: "mauricio monica cebolinha magali cascao"
+    },
+    {
+        titulo: "Turma da Mônica: Lembranças",
+        linktitulo: "http://www.guiadosquadrinhos.com/edicao/graphic-msp-(capa-dura)-n-17/gr011105/135113",
+        descricao: "Turma da Mônica: Lembranças é a emocionante conclusão da trilogia iniciada por 'Laços' e 'Lições'. Nessa graphic novel, os personagens clássicos de Mauricio de Sousa embarcam em uma jornada nostálgica pelas memórias de sua infância. A obra, escrita e ilustrada pelos irmãos Vitor e Lu Cafaggi, celebra a amizade e a importância de valorizar os momentos especiais da vida. Através de uma narrativa comovente e visualmente deslumbrante, 'Lembranças' convida os leitores a relembrarem momentos marcantes da própria infância e a refletirem sobre a importância dos laços afetivos. A graphic novel é um tributo à Turma da Mônica e um presente para todos os fãs que acompanham as aventuras dos personagens desde a primeira história.",
+        link: "https://pt.wikipedia.org/wiki/Turma_da_M%C3%B4nica:_Lembran%C3%A7as",
+        tags: "mauricio monica cebolinha magali cascao"
+    },
+    {
+        titulo: "Astronauta: Magnetar",
+        linktitulo: "http://www.guiadosquadrinhos.com/edicao/graphic-msp-(capa-dura)-n-1/gr011105/100879",
+        descricao: "Astronauta: Magnetar é a primeira graphic novel da série Graphic MSP, criada por Danilo Beyruth e com cores de Cris Peter. A história acompanha Astronauta Pereira, um solitário explorador espacial que se vê em uma situação desesperadora após sua nave ser danificada pelo poderoso campo magnético de um magnetar. Isolado no espaço, longe de casa e com seus equipamentos comprometidos, o astronauta luta contra a solidão e a loucura que começam a se instalar em sua mente. A trama explora temas como a sobrevivência, a sanidade mental e a imensidão do cosmos. Com uma arte detalhada e uma narrativa envolvente, a graphic novel nos convida a uma jornada introspectiva e repleta de suspense, mostrando os desafios e as belezas da exploração espacial.",
+        link: "https://pt.wikipedia.org/wiki/Astronauta:_Magnetar",
+        tags: "mauricio astronauta"
+    },
+    {
+        titulo: "Jeremias: Pele",
+        linktitulo: "http://www.guiadosquadrinhos.com/edicao/graphic-msp-(capa-dura)-n-18/gr011105/138654",
+        descricao: "Jeremias: Pele aborda um tema de extrema importância social: o racismo. A história acompanha Jeremias enquanto ele se depara pela primeira vez com o preconceito racial. Jeremias, um garoto negro, vive uma vida feliz e despreocupada até que começa a perceber que a cor de sua pele o coloca em uma situação diferente da de seus amigos. Através de diversas situações do dia a dia, ele vivencia o racismo de forma direta e indireta, enfrentando desde piadas até situações mais sérias de discriminação. A história, escrita por Rafael Calça e ilustrada por Jefferson Costa, explora temas como identidade, pertencimento e a importância da representatividade. Jeremias, ao longo da narrativa, precisa lidar com suas próprias dúvidas e inseguranças, buscando entender o porquê de ser tratado de forma diferente.",
+        link: "https://pt.wikipedia.org/wiki/Jeremias:_Pele",
+        tags: "mauricio jeremias racismo negro" 
+    },
+    {
+        titulo: "Penadinho: Vida",
+        linktitulo: "http://www.guiadosquadrinhos.com/edicao/graphic-msp-(capa-dura)-n-7/gr011105/116700",
+        descricao: "Penadinho: Vida aborda um tema bastante singular: o amor e a perda. A história acompanha Penadinho, o famoso fantasma da Turma da Mônica, em uma jornada emocionante e comovente. Alminha, a grande paixão de Penadinho, está prestes a reencarnar. A notícia abala o fantasma, que nunca teve coragem de confessar seus sentimentos. Em uma corrida contra o tempo, Penadinho precisa encontrar Alminha antes que ela renasça, para finalmente dizer tudo o que sente. A trama explora temas como amor, perda, saudade e a aceitação do ciclo da vida. Penadinho, ao longo da história, precisa lidar com seus próprios sentimentos e com a inevitabilidade da mudança.",
+        link: "https://universohq.com/reviews/penadinho-vida/",
+        tags: "mauricio penadinho alminha amor perda" 
+    },
+    {
+        titulo: "Chico Bento: Arvorada",
+        linktitulo: "http://www.guiadosquadrinhos.com/edicao/graphic-msp-(capa-dura)-n-15/gr011105/129730",
+        descricao: "Chico Bento: Arvorada é uma história em quadrinhos criada pelo artista Orlandeli que faz parte da coleção Graphic MSP. Nessa história, o famoso personagem caipira de Mauricio de Sousa, Chico Bento, vive uma aventura emocionante e comovente ao lado de sua avó. A trama gira em torno de um ipê amarelo, uma árvore que simboliza a passagem do tempo e a importância de aproveitar cada momento da vida. Chico Bento aprende uma lição valiosa sobre o presente e a necessidade de valorizar as pessoas que amamos. O título da história, 'Arvorada', além de se referir à árvore, também é uma brincadeira com a pronúncia caipira característica do personagem.",
+        link: "https://pt.wikipedia.org/wiki/Chico_Bento:_Arvorada",
+        tags: "mauricio chico bento alvorada tempo caipira" 
+    },
+    {
+        titulo: "Tina: Respeito",
+        linktitulo: "http://www.guiadosquadrinhos.com/edicao/graphic-msp-(capa-dura)-n-24/gr011105/148383",
+        descricao: "Tina: Respeito apresenta uma nova faceta da personagem Tina. Nessa história, criada por Fefê Torquato, Tina é uma jovem jornalista recém-formada que enfrenta um desafio inesperado: o assédio sexual no ambiente de trabalho. Ao iniciar sua carreira em uma redação, Tina se depara com Jairo Figueiredo, um jornalista experiente e respeitado que abusa de sua posição de poder para assediá-la. A história explora os sentimentos de desconforto, insegurança e impotência de Tina diante dessa situação, além de abordar as dificuldades de denunciar um superior hierárquico. Esta é uma obra que utiliza a popularidade da Turma da Mônica para abordar um tema sério e relevante, como o assédio sexual. Através da história de Tina, a obra busca conscientizar sobre essa problemática e incentivar a denúncia e o combate a esse tipo de comportamento.",
+        link: "https://pt.wikipedia.org/wiki/Tina:_Respeito",
+        tags: "mauricio tina assedio sexual" 
+    },
+    {
+        titulo: "Bidu: Caminhos",
+        linktitulo: "http://www.guiadosquadrinhos.com/edicao/graphic-msp-(capa-dura)-n-5/gr011105/111257",
+        descricao: "Bidu: Caminhos é uma emocionante jornada de amizade que reconta a história de como Bidu, o fiel companheiro de Franjinha, se tornou parte da Turma da Mônica. Criada por Eduardo Damasceno e Luís Felipe Garrocho, a história nos leva em uma jornada emocionante e cheia de aventuras, mostrando a amizade que se formou entre os dois personagens. Uma aventura cheia de problemas, surras, desvios de rota, chuva, cachorros, decisões difíceis e ternura.",
+        link: "https://en.wikipedia.org/wiki/Bidu_%E2%80%93_Caminhos",
+        tags: "mauricio bidu cachorro" 
+    },
+    {
+        titulo: "Piteco: Ingá",
+        linktitulo: "http://www.guiadosquadrinhos.com/edicao/graphic-msp-(capa-dura)-n-4/gr011105/107090",
+        descricao: "Piteco: Ingá nos transporta para a era pré-histórica, acompanhado do nosso conhecido homem das cavernas, Piteco. Criada pelo quadrinista Shiko, essa história traz uma nova perspectiva para o personagem, explorando temas como aventura, sobrevivência e a busca por um novo lar. O povo de Lem se vê obrigado a migrar porque o rio próximo à aldeia secou. Mas o valente caçador Piteco decide não ir, pois precisa resgatar Thuga, que foi raptada pela tribo dos homens-tigre. Shiko apresenta uma visão mais realista da pré-história, com elementos como a luta pela sobrevivência, a disputa por território e a importância da natureza.",
+        link: "https://pt.wikipedia.org/wiki/Piteco:_Ing%C3%A1",
+        tags: "mauricio piteco dinossauro thuga aldeia" 
+    }
+];
